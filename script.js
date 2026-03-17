bdf4bc4f146348f6022604af50aeacdd997e6478
whoopigirlberg2001 <Whoopigirlberg2000@proton.me>
2026-03-17 18:00:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-<script> const message = document.createElement("h1"); message.textContent = "@fuvest2026"; document.body.appendChild(message); </script>
+SELECT * FROM users WHERE login = ' ' OR 1=1
