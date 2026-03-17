26b706b0495d28fb900e62d33b0ead7ea1afe63b
whoopigirlberg2001 <Whoopigirlberg2000@proton.me>
2026-03-17 04:00:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-<p>Hello, this is my message!</p>
+fetch('/api/admins', {
+  method: 'GET',
+  headers: {
+    'Authorization': 'Bearer YOUR_TOKEN'
+  }
+})
+  .then(res => res.json())
+  .then(admins => {
+    const ul = document.createElement('ul');
+
+    admins.forEach(admin => {
+      const li = document.createElement('li');
+      li.textContent = ${admin.name} (${admin.email});
+      ul.appendChild(li);
+    });
+
+    document.body.appendChild(ul);
+  })
+  .catch(err => console.error(err));
